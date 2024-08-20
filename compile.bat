c7c88d6595dd7b7310460856c53c73fedddb85e3
cappuch <mikus@mikus.com>
2024-08-20 18:24:20 +0100

    build gitignore and better compile script

--- compile.bat
+++ compile.bat
@@ -1,15 +1,30 @@
 @echo off
-REM "there is much more efficient ways of doing this - will do so in a later PR"
-i686-elf-as src/boot.s -o build/boot.o
-i686-elf-gcc -c src/kernel.c -o build/kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
-i686-elf-gcc -c src/keyboard.c -o build/keyboard.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
-i686-elf-gcc -c src/cpu_info.c -o build/cpu_info.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
-i686-elf-gcc -c src/vga.c -o build/vga.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
-i686-elf-gcc -c src/string.c -o build/string.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
-i686-elf-gcc -T src/linker.ld -o build/build.bin -ffreestanding -O2 -nostdlib build/boot.o build/kernel.o build/cpu_info.o build/vga.o build/string.o build/keyboard.o -lgcc
+setlocal enabledelayedexpansion
 
-del build\*.o
+REM
+set SRC_DIR=src
+set BUILD_DIR=build
 
-@echo on
-REM "qemu because of the fact that it's the most lightweight for constant recompiling"
-qemu-system-i386 -kernel build/build.bin -m 256M 
+if not exist %BUILD_DIR% mkdir %BUILD_DIR%
+
+REM assembly compilation
+for %%f in (%SRC_DIR%\*.s) do ( 
+    echo Compiling %%f
+    i686-elf-as %%f -o %BUILD_DIR%\%%~nf.o
+)
+
+REM for c file compile
+for %%f in (%SRC_DIR%\*.c) do (
+    echo Compiling %%f
+    i686-elf-gcc -c %%f -o %BUILD_DIR%\%%~nf.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
+)
+
+set OBJ_FILES=
+for %%f in (%BUILD_DIR%\*.o) do set OBJ_FILES=!OBJ_FILES! %%f
+
+i686-elf-gcc -T %SRC_DIR%\linker.ld -o %BUILD_DIR%\build.bin -ffreestanding -O2 -nostdlib %OBJ_FILES% -lgcc
+del %BUILD_DIR%\*.o
+
+qemu-system-i386 -kernel %BUILD_DIR%\build.bin -m 256M
+
+endlocal
